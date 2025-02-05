9d44060168bb9165a5804a2ca91840654edbdd07
slacker-devlpr <shubstay@gmail.com>
2025-02-05 20:11:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,68 @@
 import streamlit as st
+# Page config:
+st.set_page_config(
+    page_title="Shaped AI, Osebni Inštruktor Matematike",
+    page_icon=r"shaped-logo.png"
+)
+
+# Load css from assets
+def load_css(file_path):
+    with open(file_path) as f:
+        st.html(f"<style>{f.read()}</style>")
+css_path = pathlib.Path("assets.css")
+load_css(css_path)
+
+# Hide all unneeded parts of streamlit:
+hide_streamlit_style = """
+<style>
+.css-hi6a2p {padding-top: 0rem;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+hide_streamlit_style = """
+<style>
+div[data-testid="stToolbar"] {
+visibility: hidden;
+height: 0%;
+position: fixed;
+}
+div[data-testid="stDecoration"] {
+visibility: hidden;
+height: 0%;
+position: fixed;
+}
+div[data-testid="stStatusWidget"] {
+visibility: hidden;
+height: 0%;
+position: fixed;
+}
+#MainMenu {
+visibility: hidden;
+height: 0%;
+}
+header {
+visibility: hidden;
+height: 0%;
+}
+footer {
+visibility: hidden;
+height: 0%;
+}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown('''
+<style>
+.stApp [data-testid="stToolbar"]{
+    display:none;
+}
+</style>
+''', unsafe_allow_html=True)
+enable_scroll = """
+<style>
+.main {
+    overflow: auto;
+}
+</style>
+"""
+st.markdown(enable_scroll, unsafe_allow_html=True)
