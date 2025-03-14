9086a1e15127c088a85de63a6e5a93f287664027
slacker-devlpr <shubstay@gmail.com>
2025-03-14 18:19:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ st.markdown(enable_scroll, unsafe_allow_html=True)
 st.title("Renaming to AnkaAI!")
 st.write("Due to trademark reasons, we have rebranded from Shaped AI to Anka AI. While our name has changed, our mission and commitment to innovation remain the same. Thank you for your support as we continue to grow under our new identity!")
 
-st.link_button("Go to AnkaAI", "https://anka-ai.streamlit.app")
+st.link_button("Go to AnkaAI", "https://anka-ai.streamlit.app", use_container_width=True)
